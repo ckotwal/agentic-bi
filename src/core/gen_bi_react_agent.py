be62df32c04675df0fc923f606a5372339318d96
sanayakotwal <kotwalsanaya@gmail.com>
2025-06-03 15:41:54 +0530

    Logging

--- src/core/gen_bi_react_agent.py
+++ src/core/gen_bi_react_agent.py
@@ -33,10 +33,11 @@ class GenBIReactAgent:
         if db_uri is None:
             DB_USER = os.getenv("DB_USER", "postgres")
             DB_PASSWORD = os.getenv("DB_PASSWORD", "password12")
-            DB_HOST = os.getenv("DB_HOST", "db")
+            DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
             DB_PORT = os.getenv("DB_PORT", "5432")
-            DB_NAME = os.getenv("DB_NAME", "chinook")
+            DB_NAME = os.getenv("DB_NAME", "postgres")
             db_uri = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        print(db_uri)
         self.db = SQLDatabase.from_uri(db_uri)
         
         # Initialize language model
